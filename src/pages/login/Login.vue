1183e37f90f2791d64786e7954c34f17fac1428c
mytpp <yourtpp@outlook.com>
2018-11-04 17:29:34 +0800

    update login.vue to vuetify

--- src/pages/login/Login.vue
+++ src/pages/login/Login.vue
@@ -1,23 +1,41 @@
 <template>
   <div id="Login">
-    <h1>长顺可视化数据库</h1>
+  
+    <v-container>
+      <v-card max-width="400">
+        <v-card-title primary-title>
+          <h1>长顺可视化数据库</h1>
 
-    <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-      <input class="mdl-textfield__input" id="adminId" type="text" 
-      v-model="LoginParams.adminId">
-      <label class="mdl-textfield__label" for="adminId">用户名</label>
-    </div>
-
-    <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-      <input class="mdl-textfield__input" id="password" type="text" 
-      v-model="LoginParams.password">
-      <label class="mdl-textfield__label" for="password">密码</label>
-    </div>
-
-    <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect"
-    @click="submit()">
-      登陆
-    </button>
+        <v-form v-model="valid">
+          <v-text-field id="adminId" 
+            v-model="LoginParams.adminId"
+            :rules="idRules"
+            prepend-inner-icon="person"
+            label="用户名"
+            required
+          ></v-text-field>
+    
+          <v-text-field id="password"
+            v-model="LoginParams.password"
+            :rules="passwordRules"
+            :append-icon="showPassword ? 'visibility_off' : 'visibility'"
+            :type="showPassword ? 'text' : 'password'"
+            prepend-inner-icon="lock" 
+            label="密码"
+            required
+            @click:append="showPassword = !showPassword"
+          ></v-text-field>
+  
+          <v-card-actions>
+            <v-btn color="primary" large block @click="submit">
+              登录
+            </v-btn>
+          </v-card-actions>
+        </v-form>
+        
+        </v-card-title>
+      </v-card>
+    </v-container>
 
   </div>
 </template>
@@ -27,36 +45,50 @@ export default {
   name: "Login",
   data() {
     return {
+      valid: true,
+      showPassword: false,
       LoginParams: {
         adminId: "",
         password: ""
-      }
-    };
+      },
+      idRules: [
+        id => !!id || '用户名必填'
+      ],
+      passwordRules: [
+        psw => !!psw || '密码必填'
+      ]
+
+    };//end return
   },
 
   methods: {
     submit: function() {
-      let formData=new URLSearchParams();
-      for(let it in this.LoginParams) {
-        formData.append(it, this.LoginParams[it]);
+      if(this.valid){
+        console.log("try submit");
+        let formData=new URLSearchParams();
+        for(let it in this.LoginParams) {
+          formData.append(it, this.LoginParams[it]);
+        }
+        console.log(formData.toString());
+        
+        this.$Http.post('https://127.0.0.1:8082/login',formData,
+        {headers:{'Content-Type':'application/x-www-form-urlencoded'}})
+        .then( 
+          response => {
+            console.log("Get Response");
+            console.log(response);
+            },
+          error => {
+            console.log("Error");
+            console.log(error);
+            }
+        );
+      } else {
+        console.log("Invalid Input");
       }
-      console.log(formData.toString());
-      
-      this.$Http.post('https://127.0.0.1:8082/login',formData,
-      {headers:{'Content-Type':'application/x-www-form-urlencoded'}})
-      .then( 
-        response => {
-          console.log("Get Response");
-          console.log(response);
-          },
-        error => {
-          console.log("Error");
-          console.log(error);
-          }
-      )
-      
-    }
+    }//end submit
   }
+
 };
 </script>
 
@@ -64,7 +96,6 @@ export default {
 <style scoped>
 #Login {
   text-align: center;
-  background-color: rgb(163, 155, 155);
   padding: 20px;
 }
 #Login div {
