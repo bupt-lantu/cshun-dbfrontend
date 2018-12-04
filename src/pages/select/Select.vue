4c700cabffce032b9d34df65490e1fdaa93407af
xiaoluo <316294622@qq.com>
2018-12-04 21:29:57 +0800

    add village detail properties

--- src/pages/select/Select.vue
+++ src/pages/select/Select.vue
@@ -43,11 +43,18 @@
             <v-card-title primary-title 
             class="village-title"
             :class="{ titleHovered: hover }">
-              <h2>{{village.name}}</h2>
+              <h2 style="font-size:22px">{{village.name}}</h2>
             </v-card-title>
-            <!-- <v-card-text>
-              <p>{{village.description}}</p>
-            </v-card-text> -->
+              <v-layout v-layout align-center justify-space-around>
+                    <p style="font-size:18px"><b>户数:</b> {{village.family}}户</p>
+                    <p style="font-size:18px"><b>人数:</b> {{ village.villagerNum }}人</p>
+                    <p style="font-size:18px"><b>系统贫困户数:</b> {{ village.sysPoorFamily }}</p>
+                    <p style="font-size:18px"><b>系统贫困人数:</b> {{ village.sysPoorVillager }}</p>
+                    <p style="font-size:18px"><b>未脱贫户数:</b> {{ village.notOutPovertyFamily }}</p>
+                    <p style="font-size:18px"><b>未脱贫人数:</b> {{ village.notOutPovertyVillager }}</p>
+                    <p style="font-size:18px"><b>低保户数:</b> {{ village.lowProtectFamily }}</p>
+                    <p style="font-size:18px"><b>低保人数:</b> {{ village.lowProtectVillager }}</p>
+              </v-layout> 
           </v-card>
         </v-hover>
       </v-flex>
