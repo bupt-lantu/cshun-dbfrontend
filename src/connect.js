81e89f875e148982a2235104347032f817b3168d
Al0ha0e <1919270783@qq.com>
2019-07-20 20:36:31 +0800

    bk

--- src/connect.js
+++ src/connect.js
@@ -1,28 +1,29 @@
-import axios from 'axios'
-import router from './router'
-axios.defaults.withCredentials=true;     //允许设置cookie
-axios.defaults.baseURL='https://dev.cshun.gaojianli.me/api';
-// axios.defaults.baseURL='http://39.98.84.18:10081/api';
+import axios from "axios";
+import router from "./router";
+axios.defaults.withCredentials = true; //允许设置cookie
+//axios.defaults.baseURL='https://dev.cshun.gaojianli.me/api';
+//axios.defaults.baseURL = "http://39.98.84.18/api";
+axios.defaults.baseURL = "http://39.98.84.18:10081/api";
 
 axios.interceptors.response.use(
-    response => {
-         return response;
-    },
-    error => {
-        if (error.response) {
-            switch (error.response.status) {
-                case 401:
-                alert("请重新登录");
-                console.log(error);
-                sessionStorage.removeItem('isLogin');
-                router.replace({
-                    path: '/login',
-                    query: {redirect: router.currentRoute.fullPath}
-                });
-            }
-        }
-        return Promise.reject(error.response.data)   // 返回接口返回的错误信息
+  response => {
+    return response;
+  },
+  error => {
+    if (error.response) {
+      switch (error.response.status) {
+        case 401:
+          alert("请重新登录");
+          console.log(error);
+          sessionStorage.removeItem("isLogin");
+          router.replace({
+            path: "/login",
+            query: { redirect: router.currentRoute.fullPath }
+          });
+      }
     }
+    return Promise.reject(error.response.data); // 返回接口返回的错误信息
+  }
 );
 
 export default axios;
